df19cb26ab3c3238b16899695dc749857cf6dfc0
skeleton <skeleton@gmail.com>
2023-01-26 11:20:04 +0100

    add state user file

---
+++ state.user.js
@@ -0,0 +1,28 @@
+import SkelX from './app.js';
+import userState from './reducers.user.js';
+
+const data = {
+  message:"hello world",
+  todos:['uno','due'],
+  users:[]
+};
+
+const myApp= new SkelX(data)
+
+userState.useState(myApp);
+myApp.subscribe(userState.reducers);
+
+
+myApp.data.message="ciao"
+myApp.data.todos.push('sposop')
+
+
+//events
+window.removeItem =(id)=>{
+  myApp.data.users=myApp.data.users.filter(u => u.id != id)
+}
+document.querySelector('#load')
+.addEventListener('click',
+()=>{
+  const users= fetch('https://jsonplaceholder.typicode.com/users').then(r => r.json()).then(u => myApp.data.users = u)
+})
