e75bf236af72307172d901cb2fd219024bbeda00
Sk3l3t0nD3v <94837716+Sk3l3t0nD3v@users.noreply.github.com>
2023-01-26 11:07:40 +0100

    refactoring for use reducers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-import App from './app.js'
+import SkelX from './app.js';
+import countState from './reducers.count.js';
+import userState from './reducers.user.js';
 
 const data = {
   message:"hello world",
@@ -7,39 +8,17 @@ const data = {
   users:[]
 };
 
-const myApp= new App(data)
+const myApp= new SkelX(data)
 
-
-const render =()=>{
-  //document.getElementById('app').innerHTML=data.message
-  const el = document.getElementById('todo').innerHTML=`
-  <h1>${data.message}</h1>
-			<ul>
-				${data.todos.map(function (todo) {
-					return `<li>${todo}</li>`;
-				}).join('')}
-			</ul>
-  `;
-
-};
-myApp.subscribe(render);
+userState.useState(myApp);
+myApp.subscribe(userState.reducers);
 
 
 myApp.data.message="ciao"
-
 myApp.data.todos.push('sposop')
-myApp.subscribe(
-  ()=>{
-    
-    const el = document.getElementById('users').innerHTML=`
-    <ul>
-				${data.users.map(function (u) {
-					return `<li onclick=removeItem(${u.id})>${u.name}</li>`;
-				}).join('')}
-		</ul>
-    `
-  }
-)
+
+
+//events
 window.removeItem =(id)=>{
   myApp.data.users=myApp.data.users.filter(u => u.id != id)
 }
@@ -51,32 +30,20 @@ document.querySelector('#load')
 
 /* COUNT APP */
 
-
-const appCount = new App({
-  items:[]
+const appCount = new SkelX({
+  items: [],
 });
 
-const cb=[
-  ()=>document.querySelector('#totale').innerHTML=`${appCount.data.items.reduce((a,{value})=> +a + +value,0)}`,
-  ()=>{
-    document.querySelector('#list').innerHTML=`
-      <ul>
-        ${appCount.data.items.map(i => `
-          <li onclick=removeItemCount(${i.id})>${i.value}</li>
-        `).join('')}
-      </ul>
-    `
-  }
-]
-appCount.subscribe(cb)
+//state management
+countState.useState(appCount);
+appCount.subscribe(countState.reducers);
 
-
-document.querySelector('#input').addEventListener('change',(e)=>{
-  const id = Math.random() * 10
-  appCount.data.items.push({id,value:e.target.value})
-  e.target.value=''
-})
-
-window.removeItemCount =(id)=>{
-  appCount.data.items=appCount.data.items.filter(u => u.id != id)
-}
+//events
+document.querySelector('#input').addEventListener('change', (e) => {
+  const id = Math.random() * 10;
+  appCount.data.items.push({ id, value: e.target.value });
+  e.target.value = '';
+});
+window.removeItemCount = (id) => {
+  appCount.data.items = appCount.data.items.filter((u) => u.id != id);
+};
