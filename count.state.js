c792f578c66265a601d8d2e19ef37bb6c97c2187
skeleton <skeleton@gmail.com>
2023-01-26 11:20:54 +0100

    add state count file

---
+++ count.state.js
@@ -0,0 +1,21 @@
+import SkelX from './app.js';
+import countState from './reducers.count.js';
+
+
+const appCount = new SkelX({
+  items: [],
+});
+
+//state management
+countState.useState(appCount);
+appCount.subscribe(countState.reducers);
+
+//events
+document.querySelector('#input').addEventListener('change', (e) => {
+  const id = Math.random() * 10;
+  appCount.data.items.push({ id, value: e.target.value });
+  e.target.value = '';
+});
+window.removeItemCount = (id) => {
+  appCount.data.items = appCount.data.items.filter((u) => u.id != id);
+};
